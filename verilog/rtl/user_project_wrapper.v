daa6c9bab58b55dc514de3f2c982fd02a56b430f
AireenAmirJalal <aireenamirjalal98@gmail.com>
2021-11-10 23:18:53 +0100

    Updated the SPDX license header

--- verilog/rtl/user_project_wrapper.v
+++ verilog/rtl/user_project_wrapper.v
@@ -1,18 +1,19 @@
 `default_nettype none
 /*
- *-------------------------------------------------------------
+* Copyright 2020 Muhammad Hadir Khan
  *
- * user_project_wrapper
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * This wrapper enumerates all of the pins available to the
- * user for the user project.
+ *      https://www.apache.org/licenses/LICENSE-2.0
  *
- * An example user project is provided in this wrapper.  The
- * example should be removed and replaced with the actual
- * user project.
- *
- *-------------------------------------------------------------
- */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
 
 module user_project_wrapper #(
     parameter BITS = 32
